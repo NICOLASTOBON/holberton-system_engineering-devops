76ac18119d55752e2fbaba062b0be4c47fe83a0e
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2020-09-14 23:44:14 -0500

    [add] files

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -17,6 +17,6 @@ if __name__ == "__main__":
                 "username": user['username'],
                 "task": todo['title'],
                 "completed": todo['completed'],
-            } for todo in todos]
+            } for todo in todos if user['id'] == todo['userId']]
     with open('todo_all_employees.json', 'w') as f:
         json.dump(new_dict, f)
