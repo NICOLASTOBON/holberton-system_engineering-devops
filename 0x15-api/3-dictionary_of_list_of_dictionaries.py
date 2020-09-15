f97cd8d219b2929dbfc7fc9cd4c590a71f666cb8
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2020-09-14 23:35:55 -0500

    [add] files

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" all json """
+import json
+import requests
+
+
+if __name__ == "__main__":
+    URL = 'https://jsonplaceholder.typicode.com/'
+
+    todos = requests.get(URL + 'todos').json()
+    users = requests.get(URL + 'users').json()
+
+    new_dict = {}
+
+    for user in users:
+        new_dict[user['id']] = [{
+                "username": user['username'],
+                "task": todo['title'],
+                "completed": todo['completed'],
+            } for todo in todos]
+    with open('todo_all_employees.json', 'w') as f:
+        json.dump(new_dict, f, indent=4)
