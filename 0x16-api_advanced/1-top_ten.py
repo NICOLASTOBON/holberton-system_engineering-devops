01643b921e682e9c03a9f526bb0a0b17e6e343a8
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2020-09-17 17:43:36 -0500

    [add] prints the titles

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" 0-main """
+
+import requests
+
+
+def top_ten(subreddit):
+    """ prints the titles """
+    URL = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    header = {'User-Agent': 'PostmanRuntime/7.26.5'}
+    data = requests.get(URL, headers=header).json().get('data')
+    data1 = data.get('children')
+
+    for count, title in enumerate(data1):
+        if count == 10:
+            break
+        print(title.get('data').get('title'))
