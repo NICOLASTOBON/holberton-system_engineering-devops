7bec2981cd8afa626ab769e23d6ed920301aa74b
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2020-09-17 17:07:03 -0500

    returns the number of subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" 0-main """
+
+import requests
+
+def number_of_subscribers(subreddit):
+    """  returns the number of subscribers """
+    URL = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    header = {'User-Agent': 'PostmanRuntime/7.26.5'}
+    data = requests.get(URL, headers=header).json()
+    result = None
+    if data.get('error') == 404:
+        result = 0
+    else:
+        data_sus = data.get('data').get('subscribers')
+        result = data_sus
+    return result
