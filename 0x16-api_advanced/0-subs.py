83d5d6992de32c932c1e3026df8da81fd24c41ab
Nicolas Alvarez Tobon <nicolas.tobon009@hotmail.com>
2020-09-17 17:08:13 -0500

    returns the number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,8 +3,9 @@
 
 import requests
 
+
 def number_of_subscribers(subreddit):
-    """  returns the number of subscribers """
+    """ returns the number of subscribers """
     URL = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     header = {'User-Agent': 'PostmanRuntime/7.26.5'}
     data = requests.get(URL, headers=header).json()
